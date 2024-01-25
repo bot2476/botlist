f3a7a1ff14045e3287df482187c28b9cd8bc5757
bot2476 <157342667+bot2476@users.noreply.github.com>
2024-01-25 22:16:58 +0800

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
+API_ID       = 18020855
+API_HASH     = "3fa7d1453b819e4aea418d9eb01997e7"
+BOT_TOKEN    = "6672899170:AAEI2ttPW17D2ZxRZKEr-IYyE9UjwnRxBr0"
+SESSION      = "BQGdbYEAZq5HCFUQOhydgrA6KYo3DHD11_aZHuDoUKtUm2dOSIKDjBBLuPhh14-nlgU_BqxggiVkOT9dFls5uc8k8_bQn05vIlyE17aUVLgHeHjb2cx-HEvG8DwsdqZcjJ1bf_ErrNpf94SAmoxp0EN0oeaR2CTYn6pYqxE8yvyi5kIi_mKPgnZUeJQO05YG0ZgqIay2oJRK2q8n6IzEns5Rgq7r68FMhT9IfYtv0PXgEUzatQjrRPXf5YrO_MaAg_tzW5oV3FQJQbGFfnwevM4o5owups_D0swlvlDgshqc-gq6uthyeZwBRn29yet4WfOxlY9L5IXpDNMiPKkNhnUoc_svxgAAAAFq8Tr4AA"
+DATABASE_URI = "mongodb+srv://movie700:movie700@cluster0.rklxod8.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001901731410 
+ADMIN        = 6747402025
 CHANNEL      = "@CyNiTeBaCkUp"
